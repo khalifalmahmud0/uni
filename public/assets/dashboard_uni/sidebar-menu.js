cf2b822838cdfff3ae7440523af7f2349a347e04
Md-shefat-masum <mshefat924@gmail.com>
2024-06-09 01:27:07 +0600

    menu active fixed

--- public/assets/dashboard_uni/sidebar-menu.js
+++ public/assets/dashboard_uni/sidebar-menu.js
@@ -1,12 +1,12 @@
 "use strict";
-$.sidebarMenu = function(menu) {
+$.sidebarMenu = function (menu) {
   var animationSpeed = 300,
-  subMenuSelector = '.sidebar-submenu';
-  $(menu).on('click', 'li a', function(e) {
+    subMenuSelector = '.sidebar-submenu';
+  $(menu).on('click', 'li a', function (e) {
     var $this = $(this);
     var checkElement = $this.next();
     if (checkElement.is(subMenuSelector) && checkElement.is(':visible')) {
-      checkElement.slideUp(animationSpeed, function() {
+      checkElement.slideUp(animationSpeed, function () {
         checkElement.removeClass('menu-open');
       });
       checkElement.parent("li").removeClass("active");
@@ -16,15 +16,15 @@ $.sidebarMenu = function(menu) {
       var ul = parent.find('ul:visible').slideUp(animationSpeed);
       ul.removeClass('menu-open');
       var parent_li = $this.parent("li");
-      checkElement.slideDown(animationSpeed, function() {
-      checkElement.addClass('menu-open');
-      parent.find('li.active').removeClass('active');
-      parent_li.addClass('active');
+      checkElement.slideDown(animationSpeed, function () {
+        checkElement.addClass('menu-open');
+        parent.find('li.active').removeClass('active');
+        parent_li.addClass('active');
       });
     }
   });
 }
-$(".mobile-sidebar .switch-state").click(function(){
-  $(".page-body-wrapper").toggleClass("sidebar-close");
-});
 $.sidebarMenu($('.sidebar-menu'));
+// $(".mobile-sidebar .switch-state").click(function(){
+//   $(".page-body-wrapper").toggleClass("sidebar-close");
+// });
