68f04e9126f77b1f83fcd0bf1ecbde60f004154d
Md-shefat-masum <mshefat924@gmail.com>
2024-06-09 15:35:44 +0600

    user details

--- public/management/super_admin/views/pages/users/Details.tsx
+++ public/management/super_admin/views/pages/users/Details.tsx
@@ -7,7 +7,7 @@ import setup from './config/setup';
 import { RootState, useAppDispatch } from '../../../store';
 import { details } from './config/store/async_actions/details';
 import { initialState } from './config/store/inital_state';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import storeSlice from './config/store';
 export interface Props {}
 
@@ -24,6 +24,16 @@ const Details: React.FC<Props> = (props: Props) => {
         dispatch(details({ id: params.id }) as any);
     }, []);
 
+    function get_value(key) {
+        try {
+            if (state.item[key]) return state.item[key];
+            if (state.item?.info[key]) return state.item?.info[key];
+        } catch (error) {
+            return '';
+        }
+        return '';
+    }
+
     return (
         <>
             <div className="page_content">
@@ -31,25 +41,72 @@ const Details: React.FC<Props> = (props: Props) => {
                     <Header page_title={setup.details_page_title}></Header>
 
                     {Object.keys(state.item).length && (
-                        <div className="content_body">
+                        <div className="content_body custom_scroll">
+                            <div className="details_page_profile_image">
+                                <img
+                                    src={
+                                        state.item.image
+                                            ? `/${state.item.image}`
+                                            : '/assets/dashboard/images/avatar.png'
+                                    }
+                                />
+                            </div>
                             <table className="table quick_modal_table table-hover">
                                 <tbody>
-                                    <tr>
-                                        <td>Name</td>
-                                        <td>:</td>
-                                        <td>{state.item.name}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Preffered Name</td>
-                                        <td>:</td>
-                                        <td>{state.item.email}</td>
-                                    </tr>
+                                    {[
+                                        'name',
+                                        'email',
+                                        'designation',
+                                        'phone_number',
+                                    ].map((i) => (
+                                        <tr>
+                                            <td>{i.replaceAll('_', ' ')}</td>
+                                            <td>:</td>
+                                            <td>{get_value(i)}</td>
+                                        </tr>
+                                    ))}
+                                    {[
+                                        'father_name',
+                                        'mother_name',
+                                        'husband_spouse',
+                                        'nid',
+                                        'education',
+                                        'permanent_address',
+                                        'present_address',
+
+                                        'bank_name',
+                                        'branch_name',
+                                        'bank_account_no',
+                                        'bank_routing_no',
+                                        'mobile_banking_portal',
+                                        'mobile_banking_ac_no',
+                                    ].map((i) => (
+                                        <tr>
+                                            <td>{i.replaceAll('_', ' ')}</td>
+                                            <td>:</td>
+                                            <td>{get_value(i)}</td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                             </table>
                         </div>
                     )}
 
-                    <Footer></Footer>
+                    <Footer>
+                        {state.item?.id && (
+                            <li>
+                                <Link
+                                    to={`/${setup.route_prefix}/edit/${state.item.id}`}
+                                    className="btn-outline-info outline"
+                                >
+                                    <span className="material-symbols-outlined fill">
+                                        edit_square
+                                    </span>
+                                    <div className="text">Edit</div>
+                                </Link>
+                            </li>
+                        )}
+                    </Footer>
                 </div>
             </div>
         </>
