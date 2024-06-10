4173be3374c7272773424e6044bfd155585da0a3
Md-shefat-masum <mshefat924@gmail.com>
2024-06-10 23:32:03 +0600

    remigrate all

--- migrate.sh
+++ migrate.sh
@@ -8,19 +8,20 @@ npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/mo
 echo "users seed end"
 echo ""
 
-# echo ""
-# echo "user-students seed start"
-# API_URL="http://127.0.0.1:5001/api/v1/user-students?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
-# response=$(curl -s "$API_URL")
-# npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/user_management/user_students/models/seeders
-# echo "user-students seed end"
-# echo ""
 
-# echo ""
-# echo "error-trace seed start"
-# API_URL="http://127.0.0.1:5001/api/v1/error-trace?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
-# response=$(curl -s "$API_URL")
-# # npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/app_management/error_trace/models/seeders
-# echo "error-trace seed end"
-# echo ""
+echo ""
+echo "contact message seed start"
+API_URL="http://127.0.0.1:5002/api/v1/contact-message?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+response=$(curl -s "$API_URL")
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/extra/contact_message/models/seeders
+echo "contact message seed end"
+echo ""
+
+echo ""
+echo "error-trace seed start"
+API_URL="http://127.0.0.1:5002/api/v1/error-trace?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+response=$(curl -s "$API_URL")
+# npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/app_management/error_trace/models/seeders
+echo "error-trace seed end"
+echo ""
 
