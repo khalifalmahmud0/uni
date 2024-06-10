09f7cee017ad848d5b21c7f3b7a2f4b860b1e614
Md-shefat-masum <mshefat924@gmail.com>
2024-06-10 23:31:15 +0600

    development steps write

---
+++ dev_steps.js
@@ -0,0 +1,46 @@
+/**
+    1. env edit
+
+    2. 
+        model make
+        src\modules
+
+        2.1. 
+            create a new management folder
+            then copy existing one
+
+        2.3 
+            route prefix set
+            src\modules\extra\contact_message\routes.ts
+
+        2.4
+            model creation
+            table name & model name setup in model.ts
+            model types
+            database columns
+
+            import into db.ts file
+            src\modules\extra\contact_message\models\db.ts
+
+            sokol service er model name thik kora
+
+    3. 
+        services all method fix
+        src\modules\extra\contact_message\services\all.ts
+        select fileds fix
+
+    4. 
+        api call
+
+    5. 
+        seeder
+        src\modules\extra\contact_message\models\seeders\contact_message_seeder.js
+
+        seeder table define
+
+    6 and 5
+        migrate.sh
+        bash migrate.sh
+
+
+ */
