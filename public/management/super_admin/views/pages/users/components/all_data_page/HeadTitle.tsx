31aab719dde137120e80ebf2f48ce6710d949df7
Md-shefat-masum <mshefat924@gmail.com>
2024-06-10 21:59:51 +0600

    dynamic title

--- public/management/super_admin/views/pages/users/components/all_data_page/HeadTitle.tsx
+++ public/management/super_admin/views/pages/users/components/all_data_page/HeadTitle.tsx
@@ -3,7 +3,9 @@ import { useSelector } from 'react-redux';
 import setup from '../../config/setup';
 import { RootState } from '../../../../../store';
 import { initialState } from '../../config/store/inital_state';
-export interface Props {}
+export interface Props {
+    title?: string;
+}
 
 const HeadTitle: React.FC<Props> = (props: Props) => {
     const state: typeof initialState = useSelector(
@@ -13,7 +15,7 @@ const HeadTitle: React.FC<Props> = (props: Props) => {
     return (
         <div className="title no_move" id="users_drag">
             <h5>
-                {setup.all_page_title}
+                {props.title ? props.title : setup.all_page_title}
 
                 {/* {state.is_loading && <span> loading..</span>} */}
             </h5>
