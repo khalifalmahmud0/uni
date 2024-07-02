b890e98c0524514fee8dc0f3c066b59b9440316c
Md-shefat-masum <mshefat924@gmail.com>
2024-07-02 14:08:21 +0600

    new corrections

---
+++ sugession.js
@@ -0,0 +1,14 @@
+/**
+ 
+1. account entry and print
+2. project entry and money receipt
+3. expense entry
+4. commission calculation
+5. site visit request
+
+6. user create e change asbe
+7. multiple nominee
+    nominee parcentange
+
+8. user form designation asignation
+ */
