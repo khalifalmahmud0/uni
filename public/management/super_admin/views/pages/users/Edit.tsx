930d2df9cafb17cc963e3ac3e06b05b14cffa1b6
Md-shefat-masum <mshefat924@gmail.com>
2024-07-08 04:54:19 +0600

    user nominee edit

--- public/management/super_admin/views/pages/users/Edit.tsx
+++ public/management/super_admin/views/pages/users/Edit.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/management_data_page/Header';
 import Footer from './components/management_data_page/Footer';
 import { useSelector } from 'react-redux';
@@ -14,20 +14,31 @@ import InputImage from './components/management_data_page/InputImage';
 import DropDown from './components/dropdown/DropDown';
 import Select from './components/management_data_page/Select';
 import { anyObject } from '../../../common_types/object';
-export interface Props {}
+import { NomineeType, set_nominee } from './helpers/nominee_helpers';
+import Nominee from './components/Nominee';
+export interface Props { }
 
 const Edit: React.FC<Props> = (props: Props) => {
     const state: typeof initialState = useSelector(
         (state: RootState) => state[setup.module_name],
     );
+    const [FormPageNominees, setFromPageNominees] = useState<NomineeType[]>([]);
 
     const dispatch = useAppDispatch();
     const params = useParams();
 
     useEffect(() => {
+        init_nominee()
         dispatch(storeSlice.actions.set_item({}));
         dispatch(details({ id: params.id }) as any);
     }, []);
+
+    function init_nominee() {
+        setFromPageNominees([
+            set_nominee(),
+            set_nominee(),
+        ]);
+    }
 
     useEffect(() => {
         if (state.item?.reference_info) {
@@ -35,11 +46,24 @@ const Edit: React.FC<Props> = (props: Props) => {
                 storeSlice.actions.set_selected([state.item.reference_info]),
             );
         }
+        // console.log(state.item);
+        if (state.item?.nominee_infos && state.item?.nominee_infos?.length) {
+            setFromPageNominees(state.item?.nominee_infos.map(i => ({
+                name: i.name,
+                relation: i.relation,
+                age: i.age,
+                percentage: i.percentage,
+                mobile_number: i.mobile_number,
+            })));
+        }
     }, [state.item]);
 
     async function handle_submit(e) {
         e.preventDefault();
-        const response = await dispatch(update(new FormData(e.target)) as any);
+        let form_data = new FormData(e.target);
+        form_data.append('nominees', JSON.stringify(FormPageNominees));
+
+        const response = await dispatch(update(form_data) as any);
     }
 
     function get_value(key) {
@@ -51,9 +75,9 @@ const Edit: React.FC<Props> = (props: Props) => {
         }
         return '';
     }
-    function get_reference(): anyObject[] | [] {
+    function get_relation_info(title): anyObject[] | [] {
         try {
-            if (state.item.reference_info) return [state.item.reference_info];
+            if (state.item[title]) return [state.item[title]];
         } catch (error) {
             return [];
         }
@@ -84,6 +108,20 @@ const Edit: React.FC<Props> = (props: Props) => {
                                     </h5>
                                     <div className="form_auto_fit">
                                         <div className="form-group form-vertical">
+                                            <Select
+                                                label="Role"
+                                                name="role"
+                                                value={state.item.role}
+                                                values={[
+                                                    { text: 'Marketing', value: 'marketing' },
+                                                    { text: 'Staff', value: 'staff' },
+                                                    { text: 'Accountant', value: 'accountant' },
+                                                    { text: 'HRM', value: 'hrm' },
+                                                    { text: 'Management', value: 'management' },
+                                                ]}
+                                            />
+                                        </div>
+                                        <div className="form-group form-vertical">
                                             <Input
                                                 name={'uid'}
                                                 label="Employee ID"
@@ -110,7 +148,7 @@ const Edit: React.FC<Props> = (props: Props) => {
                                             </div>
                                         ))}
 
-                                        <div className="form-group form-vertical">
+                                        {/* <div className="form-group form-vertical">
                                             <Select
                                                 value={state.item.designation}
                                                 label="Designation"
@@ -125,21 +163,7 @@ const Edit: React.FC<Props> = (props: Props) => {
                                                     { text: 'MO', value: 'mo' },
                                                 ]}
                                             />
-                                        </div>
-
-                                        <div className="form-group form-vertical">
-                                            <label>Reference</label>
-                                            <div className="form_elements">
-                                                <DropDown
-                                                    multiple={false}
-                                                    get_selected_data={(
-                                                        result,
-                                                    ) => console.log(result)}
-                                                    default_value={get_reference()}
-                                                    name={`reference`}
-                                                />
-                                            </div>
-                                        </div>
+                                        </div> */}
 
                                         <div className="form-group form-vertical">
                                             <Input name={'password'} />
@@ -151,6 +175,110 @@ const Edit: React.FC<Props> = (props: Props) => {
                                                 name={'image'}
                                                 defalut_preview={get_value("image")}
                                             />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <h5 className="mb-4">Nominee Informations</h5>
+                                    {
+                                        FormPageNominees.map((i, key) => (
+                                            <div key={key}>
+                                                <h6>Nominee {key + 1}</h6>
+                                                <Nominee
+                                                    FormPageNominees={FormPageNominees}
+                                                    setFromPageNominees={setFromPageNominees}
+                                                    nominee_index={key} />
+                                            </div>
+
+                                        ))
+                                    }
+                                </div>
+
+                                <div>
+                                    <h5 className="mb-4">Agent Positions</h5>
+                                    <div className="form_auto_fit">
+
+                                        <div className="form-group form-vertical">
+                                            <Select
+                                                label="Designation"
+                                                name="designation"
+                                                value={state.item.designation}
+                                                values={[
+                                                    { text: 'ED', value: 'ed' },
+                                                    { text: 'GM', value: 'gm' },
+                                                    { text: 'AGM', value: 'agm' },
+                                                    { text: 'MO', value: 'mo' },
+                                                ]}
+                                            />
+                                        </div>
+
+                                        <div className="form-group form-vertical">
+                                            <label>Joining Reference</label>
+                                            <div className="form_elements">
+                                                <DropDown
+                                                    multiple={false}
+                                                    get_selected_data={(result) => ''}
+                                                    name={`reference`}
+                                                    default_value={get_relation_info('reference_info')}
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="form-group form-vertical">
+                                            <label>
+                                                MO
+                                            </label>
+                                            <div className="form_elements">
+                                                <DropDown
+                                                    multiple={false}
+                                                    get_selected_data={(result) => ''}
+                                                    name={`mo`}
+                                                    default_value={get_relation_info('mo_info')}
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="form-group form-vertical">
+                                            <label>
+                                                AGM
+                                            </label>
+                                            <div className="form_elements">
+                                                <DropDown
+                                                    multiple={false}
+                                                    get_selected_data={(result) => ''}
+                                                    name={`agm`}
+                                                    default_value={get_relation_info('agm_info')}
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="form-group form-vertical">
+                                            <label>
+                                                GM
+                                            </label>
+                                            <div className="form_elements">
+                                                <DropDown
+                                                    multiple={false}
+                                                    get_selected_data={(result) => ''}
+                                                    name={`gm`}
+                                                    default_value={get_relation_info('gm_info')}
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="form-group form-vertical">
+                                            <label>
+                                                ED
+                                            </label>
+                                            <div className="form_elements">
+                                                <DropDown
+                                                    multiple={false}
+                                                    get_selected_data={(result) => ''}
+                                                    name={`ed`}
+                                                    default_value={get_relation_info('ed_info')}
+                                                />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
