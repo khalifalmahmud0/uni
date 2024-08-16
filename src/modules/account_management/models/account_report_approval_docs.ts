148fce86d02c6477d3d154a8faa8a0ed2b8ccb56
Md-shefat-masum <mshefat924@gmail.com>
2024-08-16 15:22:52 +0600

    account report approval model setup

---
+++ src/modules/account_management/models/account_report_approval_docs.ts
@@ -0,0 +1,84 @@
+import {
+    // Association,
+    DataTypes,
+    // HasManyAddAssociationMixin,
+    // HasManyCountAssociationsMixin,
+    // HasManyCreateAssociationMixin,
+    // HasManyGetAssociationsMixin,
+    // HasManyHasAssociationMixin,
+    // HasManySetAssociationsMixin,
+    // HasManyAddAssociationsMixin,
+    // HasManyHasAssociationsMixin,
+    // HasManyRemoveAssociationMixin,
+    // HasManyRemoveAssociationsMixin,
+    Model,
+    // ModelDefined,
+    // Optional,
+    Sequelize,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+} from 'sequelize';
+
+const tableName = 'account_report_approvals';
+const modelName = 'AccountReportApprovalModel';
+
+type Infer = InferAttributes<DataModel>;
+type InferCreation = InferCreationAttributes<DataModel>;
+type status = 'active' | 'deactive' | 'block';
+
+class DataModel extends Model<Infer, InferCreation> {
+    declare id?: CreationOptional<number>;
+
+    declare account_report_approval_id?: number;
+    declare url?: string;
+
+    declare status?: status;
+    declare creator?: number;
+
+    declare created_at?: CreationOptional<Date>;
+    declare updated_at?: CreationOptional<Date>;
+}
+
+function init(sequelize: Sequelize) {
+    DataModel.init(
+        {
+            id: {
+                type: DataTypes.BIGINT.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            account_report_approval_id: {
+                type: new DataTypes.BIGINT.UNSIGNED,
+                allowNull: true,
+            },
+            url: {
+                type: new DataTypes.DATE,
+                allowNull: true,
+            },
+
+            status: {
+                type: new DataTypes.ENUM('active', 'deactive', 'block'),
+                defaultValue: 'active',
+            },
+            creator: {
+                type: new DataTypes.TINYINT(),
+                allowNull: true,
+                defaultValue: null,
+            },
+
+            created_at: DataTypes.DATE,
+            updated_at: DataTypes.DATE,
+        },
+        {
+            tableName: tableName,
+            modelName: modelName,
+            sequelize, // passing the `sequelize` instance is required
+            underscored: true,
+        },
+    );
+
+    return DataModel;
+}
+
+export { init, DataModel };
