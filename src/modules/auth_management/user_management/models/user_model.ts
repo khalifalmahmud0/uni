cb5b7a172f8c8c6bacae03a36b30da3bf2e9a3a7
Md-shefat-masum <mshefat924@gmail.com>
2024-09-11 12:42:07 +0600

    make uid and email optional

--- src/modules/auth_management/user_management/models/user_model.ts
+++ src/modules/auth_management/user_management/models/user_model.ts
@@ -31,7 +31,7 @@ type designation = 'ed' | 'gm' | 'agm' | 'mo' | 'office_incharge';
 
 class DataModel extends Model<Infer, InferCreation> {
     declare id?: CreationOptional<number>;
-    declare uid: string;
+    declare uid?: string;
     declare name: string;
     declare role?: role;
 
@@ -42,7 +42,7 @@ class DataModel extends Model<Infer, InferCreation> {
     declare gm?: number;
     declare ed?: number;
 
-    declare email: string | null;
+    declare email?: string | null;
     declare phone_number?: string | null;
     declare image?: string | null;
 
