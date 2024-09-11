c8a0627b198de120dc81957d0a4739f747f832f4
Md-shefat-masum <mshefat924@gmail.com>
2024-09-11 12:37:41 +0600

    save success payment and print

--- public/management/customer/views/pages/Payment/PaymentResponse.tsx
+++ public/management/customer/views/pages/Payment/PaymentResponse.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { makePayment, verifyPayment } from "shurjopay-js";
 import { anyObject } from '../../../../mo/common_types/object';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 export interface Props { }
 
 const T1: React.FC<Props> = (props: Props) => {
     const [paymentInfo, setPaymentInfo] = useState<anyObject>({});
+    const [invoiceInfo, setInvoiceInfo] = useState<anyObject>({});
+    const [isProcessing, setIsProcessing] = useState<boolean>(true);
     const [params] = useSearchParams();
 
     useEffect(() => {
@@ -15,16 +17,32 @@ const T1: React.FC<Props> = (props: Props) => {
     }, [])
 
     async function verify_payment(order_id) {
-        let payment = await verifyPayment(order_id);
+        let payment = await verifyPayment(order_id) as any;
         if (payment.length) {
             setPaymentInfo(payment[0]);
             console.log(payment[0]);
-            if(payment[0].bank_status == "Success"){
-                axios.post('/api/v1/account/logs/store-gateway-payment-on-success',{
+            if (payment[0].bank_status == "Success") {
+                axios.post('/api/v1/account/logs/store-gateway-payment-on-success', {
                     ...payment[0],
                 })
+                    .then(res => {
+                        try {
+                            let payment = res.data.data.payment;
+                            localStorage.setItem('booking', JSON.stringify(payment))
+                            setInvoiceInfo(payment);
+                            setIsProcessing(false);
+                        } catch (error) {
+
+                        }
+                    })
+            }else{
+                setIsProcessing(false);
             }
         }
+    }
+
+    function print_invoice() {
+        window.open("/print-customer-payment-invoice?id=" + invoiceInfo?.id, '_blank');
     }
 
     return <div className="container">
@@ -33,17 +51,27 @@ const T1: React.FC<Props> = (props: Props) => {
                 <div className="card w-100 mt-3">
                     <div className="card-body">
                         {
-                            paymentInfo &&
+                            (paymentInfo && !isProcessing) &&
                             <div>
                                 {
                                     paymentInfo.bank_status == "Success" ?
-                                    <h1 className="text-success">
-                                        {paymentInfo.bank_status}
-                                    </h1>
-                                    :
-                                    <h1 className="text-danger">
-                                        {paymentInfo.bank_status}
-                                    </h1>
+                                        <div>
+                                            <h1 className="text-success">
+                                                {paymentInfo.bank_status}
+                                            </h1>
+                                            <button onClick={print_invoice} className="btn btn-info" type='button'>
+                                                Print Invoice
+                                            </button>
+                                        </div>
+                                        :
+                                        <div>
+                                            <h1 className="text-danger">
+                                                transaction failed
+                                            </h1>
+                                            <Link to={'/'} className="btn btn-warning">
+                                                Please Try Again
+                                            </Link>
+                                        </div>
                                 }
                             </div>
                         }
