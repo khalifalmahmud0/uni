63d27b5af70da7597a9fd2f55fd448e1d002e268
Md-shefat-masum <mshefat924@gmail.com>
2024-09-11 12:40:30 +0600

    fix middleware conditions

--- src/modules/auth_management/authetication/services/check_auth.ts
+++ src/modules/auth_management/authetication/services/check_auth.ts
@@ -10,19 +10,22 @@ const check_auth = async (request: FastifyRequest, reply: FastifyReply) => {
     const token = request.cookies.token;
     const user_agent = request.headers['user-agent'];
 
+    
     if (!token || !token.startsWith('Bearer ')) {
         reply.code(401).send({ error: 'Unauthorized' });
         return;
     }
-
+    
     try {
         const decoded = jwt.verify(token.slice(7), secretKey);
         let models = await db();
         let user = await models.UserModel.findByPk(decoded.id);
+        console.log(user, );
+        
         if (
             user &&
-            user.token == decoded.token &&
-            decoded.user_agent == user_agent
+            user.token == decoded.token
+            // && decoded.user_agent == user_agent
         ) {
             (request as anyObject).user = user;
             return;
