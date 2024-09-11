9ed831ce2b961b21959ef6437546945db85fe95e
Md-shefat-masum <mshefat924@gmail.com>
2024-09-11 13:50:19 +0600

    list all customers

--- public/management/mo/views/pages/Customer/AllCustomer.tsx
+++ public/management/mo/views/pages/Customer/AllCustomer.tsx
@@ -1,48 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { anyObject } from '../../../../customer/common_types/object';
+import axios from 'axios';
 export interface Props { }
 
 const AllCustomer: React.FC<Props> = (props: Props) => {
+    const [customers, setCustomers] = useState<anyObject[]>([])
+    useEffect(() => {
+        axios.get('/api/v1/users/user-refered-customers')
+            .then(res => {
+                setCustomers(res.data.data);
+            })
+    }, [])
+
     return <div className="page_content">
         <div className="row">
             <div className="col-12">
                 <div className="card w-100 mt-4">
                     <div className="card-header">
                         <h5>
-                            Customers
+                            Customers ( {customers.length} Persons )
                         </h5>
                         <div className="card-header-right">
                         </div>
                     </div>
                     <div className="card-body">
-                        <div className="user-status height-scroll custom-scrollbar">
-                            <table className="table table-bordernone">
-                                <thead>
+                        <div className="user-status height-scroll custom-scrollbar" style={{maxHeight: "calc(100vh - 246px)"}}>
+                            <table className="table table-bordernone  table-hover">
+                                <thead className="bg-dark position-sticky top-0">
                                     <tr>
-                                        <th scope="col" className="pt-0">Project</th>
                                         <th scope="col" className="pt-0">Name</th>
+                                        <th scope="col" className="pt-0">Code</th>
+                                        <th scope="col" className="pt-0">Phone Number</th>
+                                        <th scope="col" className="pt-0">Project</th>
                                         <th scope="col" className="pt-0">Date</th>
-                                        <th scope="col" className="pt-0">Total Paid</th>
-                                        <th scope="col" className="pt-0">Total Revenue</th>
+                                        <th scope="col" className="pt-0">Have to Pay</th>
+                                        <th scope="col" className="pt-0">Booking Money</th>
+                                        <th scope="col" className="pt-0">Down Payment</th>
+                                        <th scope="col" className="pt-0">Installment</th>
+                                        <th scope="col" className="pt-0">Total</th>
+                                        <th scope="col" className="pt-0">Due</th>
                                         <th scope="col" className="pt-0"></th>
                                         <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {
-                                        [40000, 55000, 24000, 60000].map(el => {
-
+                                        customers.map((item: anyObject) => {
+                                            let project_info = item.project_customer_information?.project_info;
+                                            let customer_info: anyObject = {};
+                                            try {
+                                                customer_info = JSON.parse(item.project_customer_information.customer_informations);
+                                            } catch (error) {
+                                                customer_info = item.project_customer_information.customer_informations;
+                                            }
                                             return (<tr>
                                                 <td>
-                                                    Basundara river view
+                                                    {item.name}
                                                 </td>
                                                 <td>
-                                                    Abdullah al Mahmood
+                                                    {item.uid}
+                                                </td>
+                                                <td>
+                                                    <a href={"tel:" + item.phone_number}>
+                                                        {item.phone_number}
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    {project_info.title}
                                                 </td>
                                                 <td className="digits">
-                                                    {new Date().toDateString()}
+                                                    {new Date(customer_info.date).toDateString()}
                                                 </td>
-                                                <td className="digits">{el} TK</td>
-                                                <td className="digits">{el - 500} TK</td>
+                                                <td className="digits">
+                                                    {customer_info.have_to_pay_amount} TK
+                                                </td>
+                                                <td className="digits">
+                                                    {item.total_booking_money} TK
+                                                </td>
+                                                <td className="digits">
+                                                    {item.total_down_payment} TK
+                                                </td>
+                                                <td className="digits">
+                                                    {item.total_installment} TK
+                                                </td>
+                                                <td className="digits">
+                                                    {item.total_paid} TK
+                                                </td>
+                                                <td className="digits">
+                                                    {customer_info.have_to_pay_amount - item.total_paid} TK
+                                                </td>
                                                 <td>
                                                     <button className="btn btn-info">
                                                         Payment Histories
